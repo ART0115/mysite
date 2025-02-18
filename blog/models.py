07cb7c659738d78d3b91e70e19e07d68bdec55da
Atupye Tweve <atutweve64@gmail.com>
2025-02-18 21:03:21 +0300

    initial commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
@@ -6,8 +7,14 @@ class Post(models.Model):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
+
     title = models.CharField(max_length = 255)
     slug = models.SlugField(max_length = 250)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts',
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -20,6 +27,8 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-publish']
+        indexes = [models.Index(fields=['-publish']),
+        ]
 
     def __str__(self):
         return self.title #return a string with the human-readable representation of the object
